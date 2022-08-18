7d6501d7c2c973c958c3644f6a470070657f431d
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-08-18 19:16:07 +0300

    Added trade pile connection

---
+++ Magnus.Futbot.Selenium/Trading/Connections/TradePileConnection.cs
@@ -0,0 +1,24 @@
+﻿using Magnus.Futbot.Common.Models.Selenium.Trading;
+using Magnus.Futbot.Selenium.Trading.Connections.Base;
+using Newtonsoft.Json;
+
+namespace Magnus.Futbot.Selenium.Trading.Connections
+{
+    public class TradePileConnection : GetConnection
+    {
+        public TradePileConnection(HttpClient httpClient) : base(httpClient)
+        {
+            BaseAddress = new Uri("https://utas.external.s2.fut.ea.com/ut/game/fifa22/tradepile");
+        }
+
+        public async Task<TradePile?> GetTradePileAsync(string utsid)
+        {
+            UTSID = utsid;
+            var response = await GetAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return JsonConvert.DeserializeObject<TradePile>(await response.Content.ReadAsStringAsync());
+
+            return null;
+        }
+    }
+}
