7aaa43ef2e99ab0bf2bbf4c67f4b6f86b628f5ea
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-08-22 19:54:51 +0300

    Removed Credits

--- Magnus.Futbot.Common/Models/Selenium/Trading/TradePile.cs
+++ Magnus.Futbot.Common/Models/Selenium/Trading/TradePile.cs
@@ -1,19 +1,18 @@
 ﻿namespace Magnus.Futbot.Common.Models.Selenium.Trading
 {
+    public class TradePile
+    {
+        public List<PlayerCard> TransferList { get; set; } = new List<PlayerCard>();
+        public List<PlayerCard> UnassignedItems { get; set; } = new List<PlayerCard>();
+        public List<PlayerCard> TransferTargets { get; set; } = new List<PlayerCard>();
+        public List<PlayerCard> ClubItems { get; set; } = new List<PlayerCard>();
+    }
+
     public class PlayerCard
     {
         public string Name { get; set; } = string.Empty;
         public int Rating { get; set; }
         public PlayerCardStatus PlayerCardStatus {get; set;}
         public PlayerCardType PlayerType { get; set; }
-    }
-
-    public class TradePile
-    {
-        public int Credits { get; set; }
-        public List<PlayerCard> TransferList { get; set; } = new List<PlayerCard>();
-        public List<PlayerCard> UnassignedItems { get; set; } = new List<PlayerCard>();
-        public List<PlayerCard> TransferTargets { get; set; } = new List<PlayerCard>();
-        public List<PlayerCard> ClubItems { get; set; } = new List<PlayerCard>();
     }
 }
