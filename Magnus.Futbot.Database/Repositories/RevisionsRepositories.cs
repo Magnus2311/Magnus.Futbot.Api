88ee29b4750278049968729d0f0c1e18312e9681
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-08-28 15:01:27 +0300

    Created Revision

---
+++ Magnus.Futbot.Database/Repositories/RevisionsRepositories.cs
@@ -0,0 +1,12 @@
+﻿using Magnus.Futbot.Database.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Magnus.Futbot.Database.Repositories
+{
+    public class RevisionsRepository : BaseRepository<Revision>
+    {
+        public RevisionsRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+    }
+}
