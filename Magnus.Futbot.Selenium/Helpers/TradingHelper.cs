4d409e122fd4c94ffdf772cfdf7717f5bf7001df
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-09-09 22:08:05 +0300

    Trading helper added

---
+++ Magnus.Futbot.Selenium/Helpers/TradingHelper.cs
@@ -0,0 +1,19 @@
+﻿using Magnus.Futbot.Common;
+
+namespace Magnus.Futbot.Selenium.Helpers
+{
+    public static class TradingHelper
+    {
+        public static PromoType GetPromoTypeByRevision(string revision)
+        {
+            switch (revision)
+            {
+                case "Icon Moments":
+                case "Icon":
+                    return PromoType.Icon;
+                default:
+                    return PromoType.Gold;
+            }
+        }
+    }
+}
