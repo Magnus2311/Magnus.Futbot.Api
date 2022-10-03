8aed1eaf56a76358ca119d6071d310e970294e7c
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-10-03 17:54:55 +0300

    Trade actions hub added

---
+++ Magnus.Futbot.Api/Hubs/ActionsHub.cs
@@ -0,0 +1,31 @@
+﻿using Magnus.Futbot.Api.Hubs.Interfaces;
+using Magnus.Futbot.Api.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Magnus.Futbot.Api.Hubs
+{
+    public class ActionsHub : Hub<IActionsClient>
+    {
+        private readonly ActionsService _actionsService;
+
+        public ActionsHub(ActionsService actionsService)
+        {
+            _actionsService = actionsService;
+        }
+
+        public async Task GetAllActionsByProfileId(string profileId)
+        {
+            var actions = await _actionsService.GetPendingActionsByProfileId(profileId);
+
+            var userId = Context.UserIdentifier ?? "";
+            await Clients.All.OnActionsLoaded(actions);
+        }
+
+        public async Task CancelActionById(string profileId, string actionId)
+        {
+            await _actionsService.CancelActionById(profileId, actionId);
+            var userId = Context.UserIdentifier ?? "";
+            await Clients.Users(userId).OnActionCanceled(actionId);
+        }
+    }
+}
---
+++ Magnus.Futbot.Api/Hubs/Interfaces/IActionsClient.cs
@@ -0,0 +1,10 @@
+﻿using Magnus.Futbot.Common.Models.DTOs.Trading.Actions;
+
+namespace Magnus.Futbot.Api.Hubs.Interfaces
+{
+    public interface IActionsClient
+    {
+        Task OnActionCanceled(string actionId);
+        Task OnActionsLoaded(IEnumerable<TradeActionDTO> actions);
+    }
+}
