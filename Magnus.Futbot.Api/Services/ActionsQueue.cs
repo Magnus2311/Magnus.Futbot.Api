a98f3f5e0dddc13ba46079a2176848c359eed600
yavor.orlyov <yavor.orlyov@microinvest.net>
2023-09-29 20:18:28 +0300

    Промяна на action-ите

---
+++ Magnus.Futbot.Api/Services/ActionsQueue.cs
@@ -0,0 +1,205 @@
+﻿using Magnus.Futbot.Common;
+using Magnus.Futbot.Common.Models.Selenium.Actions;
+
+namespace Magnus.Futbot.Api.Services
+{
+    public class ActionsQueue
+    {
+        private readonly ActionsService _actionsService;
+        private readonly object _locker = new();
+
+        public ActionsQueue(ActionsService actionsService)
+        {
+            _actionsService = actionsService;
+        }
+
+        public PriorityQueue<TradeAction, int> PendingActions { get; set; } = new();
+
+        public TradeAction AddAction(BuyAction action)
+        {
+            lock (_locker)
+            {
+                if (PendingActions.Count > 0)
+                {
+                    var tradeAction = new BuyAction(action.ProfileId, new Func<Task>(async () =>
+                    {
+                        try
+                        {
+                            await action.Action.Invoke();
+                        }
+                        catch
+                        {
+
+                        }
+
+                        if (PendingActions.TryDequeue(out var nextAction, out _))
+                        {
+                            if (!nextAction.CancellationTokenSource.Token.IsCancellationRequested)
+                            {
+                                await nextAction.Action.Invoke();
+                                await _actionsService.DeactivateAction(nextAction.Id, TradeActionType.Buy);
+                            }
+                        }
+                    }), action.CancellationTokenSource, action.BuyCardDTO);
+
+                    PendingActions.Enqueue(tradeAction, action.Priority);
+                    return tradeAction;
+                }
+                else
+                {
+                    var tradeAction = new BuyAction(action.ProfileId, new Func<Task>(async () =>
+                    {
+                        try
+                        {
+                            await action.Action.Invoke();
+                        }
+                        catch
+                        {
+
+                        }
+                        PendingActions.TryDequeue(out _, out _);
+                        if (PendingActions.TryDequeue(out var nextAction, out _))
+                        {
+                            if (!nextAction.CancellationTokenSource.Token.IsCancellationRequested)
+                            {
+                                await nextAction.Action.Invoke();
+                            }
+                        }
+                    }), action.CancellationTokenSource, action.BuyCardDTO);
+
+                    PendingActions.Enqueue(tradeAction, tradeAction.Priority);
+                    Task.Run(async () =>
+                    {
+                        await tradeAction.Action.Invoke();
+                        await _actionsService.DeactivateAction(tradeAction.Id, TradeActionType.Buy);
+                    });
+                    return tradeAction;
+                }
+            }
+        }
+
+        public TradeAction AddAction(SellAction action)
+        {
+            lock (_locker)
+            {
+                if (PendingActions.Count > 0)
+                {
+                    var tempAction = new SellAction(action.ProfileId, new Func<Task>(async () =>
+                    {
+                        try
+                        {
+                            await action.Action.Invoke();
+                        }
+                        catch
+                        {
+
+                        }
+
+                        if (PendingActions.TryDequeue(out var nextAction, out _))
+                        {
+                            if (!nextAction.CancellationTokenSource.Token.IsCancellationRequested)
+                            {
+                                await nextAction.Action.Invoke();
+                                await _actionsService.DeactivateAction(nextAction.Id, TradeActionType.Sell);
+                            }
+                        }
+                    }), action.CancellationTokenSource, action.SellCardDTO);
+                    PendingActions.Enqueue(tempAction, action.Priority);
+                    return tempAction;
+                }
+                else
+                {
+                    var tempAction = new SellAction(action.ProfileId, new Func<Task>(async () =>
+                    {
+                        try
+                        {
+                            await action.Action.Invoke();
+                        }
+                        catch
+                        {
+
+                        }
+                        PendingActions.TryDequeue(out _, out _);
+                        if (PendingActions.TryDequeue(out var nextAction, out _))
+                        {
+                            if (!nextAction.CancellationTokenSource.Token.IsCancellationRequested)
+                            {
+                                await nextAction.Action.Invoke();
+                            }
+                        }
+                    }), action.CancellationTokenSource, action.SellCardDTO);
+
+                    PendingActions.Enqueue(tempAction, tempAction.Priority);
+                    Task.Run(async () =>
+                    {
+                        await tempAction.Action.Invoke();
+                        await _actionsService.DeactivateAction(tempAction.Id, TradeActionType.Sell);
+                    });
+                    return tempAction;
+                }
+            }
+        }
+
+        public TradeAction AddAction(MoveAction action)
+        {
+            lock (_locker)
+            {
+                if (PendingActions.Count > 0)
+                {
+                    var tempAction = new MoveAction(action.ProfileId, new Func<Task>(async () =>
+                    {
+                        try
+                        {
+                            await action.Action.Invoke();
+                        }
+                        catch
+                        {
+
+                        }
+
+                        if (PendingActions.TryDequeue(out var nextAction, out _))
+                        {
+                            if (!nextAction.CancellationTokenSource.Token.IsCancellationRequested)
+                            {
+                                await nextAction.Action.Invoke();
+                                await _actionsService.DeactivateAction(nextAction.Id, TradeActionType.Move);
+                            }
+                        }
+                    }), action.CancellationTokenSource, action.Description);
+                    PendingActions.Enqueue(tempAction, action.Priority);
+                    return tempAction;
+                }
+                else
+                {
+                    var tempAction = new MoveAction(action.ProfileId, new Func<Task>(async () =>
+                    {
+                        try
+                        {
+                            await action.Action.Invoke();
+                        }
+                        catch
+                        {
+
+                        }
+                        PendingActions.TryDequeue(out _, out _);
+                        if (PendingActions.TryDequeue(out var nextAction, out _))
+                        {
+                            if (!nextAction.CancellationTokenSource.Token.IsCancellationRequested)
+                            {
+                                await nextAction.Action.Invoke();
+                            }
+                        }
+                    }), action.CancellationTokenSource, action.Description);
+
+                    PendingActions.Enqueue(tempAction, tempAction.Priority);
+                    Task.Run(async () =>
+                    {
+                        await tempAction.Action.Invoke();
+                        await _actionsService.DeactivateAction(tempAction.Id, TradeActionType.Move);
+                    });
+                    return tempAction;
+                }
+            }
+        }
+    }
+}
